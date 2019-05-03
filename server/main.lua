ca311baadc56152c76451591dbaeccf0c87f370c
tonnizi <38116869+tonnizi@users.noreply.github.com>
2019-05-03 23:55:55 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -8,7 +8,7 @@ AddEventHandler('esx-ecotrash:sellBottles', function()
     local xPlayer = ESX.GetPlayerFromId(src)
 
     local current = xPlayer.getInventoryItem('bottle').count
-    local randomMoney = math.random(1, 4)
+    local randomMoney = math.random(1, 2)
 
     if current > 0 then
         xPlayer.removeInventoryItem('bottle', current)
@@ -26,40 +26,49 @@ AddEventHandler('esx-ecotrash:retrieveBottle', function()
     local xPlayer = ESX.GetPlayerFromId(src)
 
     local luck = math.random(0, 69)
-    local random = math.random(1, 4)
+    local random = math.random(1, 2)
 
     if luck >= 0 and luck <= 29 then
         TriggerClientEvent('esx:showNotification', src, 'Roskakorissa ei ole mitään!')
     else
         xPlayer.addInventoryItem('bottle', random)
-        TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Pullon')
-        if luck == 27 then
-            xPlayer.addInventoryItem('ruuti', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Ruutia')
-        elseif luck == 31 then
-            xPlayer.addInventoryItem('diamond', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Timanttin')
+        TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista pulloja')
+        if luck == 33 then
+			xPlayer.addInventoryItem('bread', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Leivän')
         elseif luck == 35 then
-            xPlayer.addInventoryItem('petrol', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Bensan')
-        elseif luck == 39 then
-            xPlayer.addInventoryItem('fixkit', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Korjaussarjan')
-        elseif luck == 43 then
-            xPlayer.addInventoryItem('blowpipe', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Polttoleikkurin')
-        elseif luck == 47 then
-            xPlayer.addInventoryItem('weed', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Kannabista')
-        elseif luck == 51 then
-            xPlayer.addInventoryItem('donut', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Donitsi')
-        elseif luck == 55 then
-            xPlayer.addInventoryItem('bread', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Leipä')
-        elseif luck == 59 then
-            xPlayer.addInventoryItem('jager', random)
-            TriggerClientEvent('esx:showNotification', src, 'Löysit roskiksesta ' .. random .. ' Pontikka shotin')
-        end        
+            xPlayer.addInventoryItem('donut', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Donitsin')
+        elseif luck == 34 then
+            xPlayer.addInventoryItem('muovi', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Muovin')
+        elseif luck == 38 then
+            xPlayer.addInventoryItem('petrol', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1  Öljyn')
+        elseif luck == 42 then
+        	xPlayer.addInventoryItem('haisevasukka', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Haisevan sukan')
+        elseif luck == 46 then
+            xPlayer.addInventoryItem('ruuti', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Paketin ruutia')
+        elseif luck == 50 then
+            xPlayer.addInventoryItem('blowpipe', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Polttoleikkurin')
+        elseif luck == 54 then
+            xPlayer.addInventoryItem('weapon_bottle', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Rikkinäisen pullon')
+        elseif luck == 58 then
+            xPlayer.addInventoryItem('marijuana', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Marijuanan')
+        elseif luck == 62 then
+            xPlayer.addInventoryItem('diamond', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Timanttin')
+        elseif luck == 66 then
+            xPlayer.addInventoryItem('fixkit', 1)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 1 Korjauskitin')
+        elseif luck == 69 then
+            xPlayer.addInventoryItem('teippi', 2)
+            TriggerClientEvent('esx:showNotification', src, 'Löysit roskakorista 2 Teippiä')
+        end
     end
 end)
