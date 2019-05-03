3bd4e73bbcab408fdff657fa923e7030ecfc8a1c
tonnizi <38116869+tonnizi@users.noreply.github.com>
2019-05-03 23:55:34 +0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -13,31 +13,31 @@ end)
 
 local Coordinates = {
     { x = 29.337753295898, y = -1770.3348388672, z = 29.607357025146 - 0.945 },
-    { x = 388.30194091797, y = -874.88238525391, z = 29.295169830322 - 0.945 },
-    { x = 26.877752304077, y = -1343.0764160156, z = 29.497024536133 - 0.945 },
+    --{ x = 388.30194091797, y = -874.88238525391, z = 29.295169830322 - 0.945 },
+    --{ x = 26.877752304077, y = -1343.0764160156, z = 29.497024536133 - 0.945 },
 }
 
 function LoadMarkers()
-    Citizen.Wait(0)
+    Citizen.Wait(1000)
 
     Citizen.CreateThread(function()
 
         for index, value in pairs(Coordinates) do
             local blip = AddBlipForCoord(value.x, value.y, value.z)
     
-            SetBlipSprite (blip, 409)
+            SetBlipSprite (blip, 365)
             SetBlipDisplay(blip, 4)
             SetBlipScale  (blip, 0.8)
-            SetBlipColour (blip, 48)
+            SetBlipColour (blip, 20)
             SetBlipAsShortRange(blip, true)
     
             BeginTextCommandSetBlipName("STRING")
-            AddTextComponentString('Palauta')
+            AddTextComponentString('Palautus')
             EndTextCommandSetBlipName(blip)
         end
 
         while true do
-            Citizen.Wait(5)
+            sleep = 500
 
             local x, y, z = table.unpack(GetEntityCoords(PlayerPedId()))
 
@@ -47,9 +47,10 @@ function LoadMarkers()
                 local distance = GetDistanceBetweenCoords(x, y, z, val.x, val.y, val.z, true)
 
                 if distance <= 5.0 then
+                    sleep = 1
                     DrawMarker(27, val.x, val.y, val.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.5, 1.5, 1.0, 255, 255, 255, 100, false, true, 2, false, false, false, false)
                     if distance <= 1.5 then
-                        ESX.Game.Utils.DrawText3D({x = val.x, y = val.y, z = val.z + 1}, '[E] Palautus', 0.4)
+                        ESX.Game.Utils.DrawText3D({x = val.x, y = val.y, z = val.z + 1}, '[E] Pullojen palautus', 0.4)
                         if IsControlJustReleased(0, 38) then
                             TriggerServerEvent('esx-ecotrash:sellBottles')
                         end
@@ -57,40 +58,38 @@ function LoadMarkers()
                 end
 
             end
+            Citizen.Wait(sleep)
         end
     
     end)
 end
 
+trashCanModels = {
+  [1] = -2096124444,
+  [2] = 218085040,
+  [3] = -58485588,
+  [4] = -329415894
+}
+
 Citizen.CreateThread(function()
     while true do
 
-        Citizen.Wait(5)
-
-        local entity, distance = ESX.Game.GetClosestObject({
-            'prop_bin_01a',
-            'prop_bin_03a',
-            'prop_bin_05a',
-        })
-
-        if distance ~= -1 and distance <= 1.5 then
-            if entity ~= nil then
-                local binCoords = GetEntityCoords(entity)
-                ESX.Game.Utils.DrawText3D({ x = binCoords.x, y = binCoords.y, z = binCoords.z + 1 }, '[E] Etsi Roskakori', 0.4)
-                if IsControlJustReleased(0, 38) then
-                    if lastBin ~= entity then
-                        lastBin = entity
-                        OpenTrashCan()
-                    else
-                        ESX.ShowNotification('Tarkastit jo roskiksen')
-                    end
+        sleep = 500
+        local trashCan = closestTrashCan()
+        if trashCan ~= 0 and trashCan ~= nil then
+            sleep = 1
+            local binCoords = GetEntityCoords(trashCan)
+            ESX.Game.Utils.DrawText3D({ x = binCoords.x, y = binCoords.y, z = binCoords.z + 1 }, '[E] Dyykkaus', 0.4)
+            if IsControlJustReleased(0, 38) then
+                if lastBin ~= trashCan then
+                    lastBin = trashCan
+                    OpenTrashCan()
+                else
+                    ESX.ShowNotification('Tarkastit jo roskiksen')
                 end
-            else
-                Citizen.Wait(500)
             end
-        else
-            Citizen.Wait(1000)
         end
+        Citizen.Wait(sleep)
     end
 end)
 
@@ -99,4 +98,20 @@ function OpenTrashCan()
     Citizen.Wait(10000)
     TriggerServerEvent('esx-ecotrash:retrieveBottle')
     ClearPedTasksImmediately(PlayerPedId())
+end
+
+function closestTrashCan()
+  local ped = GetPlayerPed(-1)
+  local pedCoords = GetEntityCoords(ped)
+
+  for i = 1, #trashCanModels do
+
+      local closestTrashCan = GetClosestObjectOfType(pedCoords.x, pedCoords.y, pedCoords.z, 1.5, trashCanModels[i], false, false)
+      
+      if closestTrashCan ~= nil then
+        if GetDistanceBetweenCoords(pedCoords, GetEntityCoords(closestTrashCan)) <= 1.5 then
+          return closestTrashCan
+      end
+    end
+  end
 end
